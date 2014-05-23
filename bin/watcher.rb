50c516aa3f92c574cf5af34bfd925e305b1be7f2
Matt Garriott <matt.garriott@gmail.com>
2014-05-23 13:08:02 -0600

    Send a notification if any fatal errors occur

--- bin/watcher.rb
+++ bin/watcher.rb
@@ -24,42 +24,50 @@ log("Started successfully, Min Temp = #{config[:min_safe_temp]}, " +
 
 below_freezing = false
 last_date = Date.today()
-while(true)
-  min_temp = Weather.get_min_temp(config[:zip_code])
-  today = Date.today()
 
-  log("Weather data gathered. Minimum temperature = #{min_temp}")
+notifier = Notifier.new(
+  auth_file: AuthenticationFile.new(File.join(__dir__, '..', '.auth_file')),
+  server_address: config[:server_address],
+  port: config[:port]
+)
 
-  if (last_date != today)
-    log("New day beginning...")
-    # We are on to a new day reset the variable
-    below_freezing = false
+begin
+  while(true)
+    min_temp = Weather.get_min_temp(config[:zip_code])
+    today = Date.today()
+
+    log("Weather data gathered. Minimum temperature = #{min_temp}")
+
+    if (last_date != today)
+      log("New day beginning...")
+      # We are on to a new day reset the variable
+      below_freezing = false
+    end
+
+    if min_temp.to_i < config[:min_safe_temp].to_i && !below_freezing
+      log("Temperature below safe minimum. Sending alert email...")
+
+      notifier.send(config[:to], 'FreezeWatch: Frost Warning!',
+        "The prospective minimum temperature has dropped below safe limits.\n" +
+        "Prospective temperature: #{min_temp} degrees at #{Time.now}")
+      below_freezing = true
+    elsif min_temp.to_i >= config[:min_safe_temp].to_i && below_freezing
+      log("Temperature now above safe minimum. Sending info email...")
+
+      notifier.send(config[:to], 'FreezeWatch: Temperature Raised',
+        "You might be off the hook. The predicted minimum temperature has " +
+        "climbed above your minimum safe temperature.\n" +
+        "Prospective temperature: #{min_temp} degrees at #{Time.now}")
+      below_freezing = false
+    end
+
+    last_date = today
+
+    sleep(config[:sleep_time])
   end
-
-  notifier = Notifier.new(
-    auth_file: AuthenticationFile.new(File.join(__dir__, '..', '.auth_file')),
-    server_address: config[:server_address],
-    port: config[:port]
-  )
-
-  if min_temp.to_i < config[:min_safe_temp].to_i && !below_freezing
-    log("Temperature below safe minimum. Sending alert email...")
-
-    notifier.send(config[:to], 'FreezeWatch: Frost Warning!',
-      "The prospective minimum temperature has dropped below safe limits.\n" +
-      "Prospective temperature: #{min_temp} degrees at #{Time.now}")
-    below_freezing = true
-  elsif min_temp.to_i >= config[:min_safe_temp].to_i && below_freezing
-    log("Temperature now above safe minimum. Sending info email...")
-
-    notifier.send(config[:to], 'FreezeWatch: Temperature Raised',
-      "You might be off the hook. The predicted minimum temperature has " +
-      "climbed above your minimum safe temperature.\n" +
-      "Prospective temperature: #{min_temp} degrees at #{Time.now}")
-    below_freezing = false
-  end
-
-  last_date = today
-
-  sleep(config[:sleep_time])
+rescue => e
+  notifier.send(config[:to], 'FreezeWatch: Something Went Wrong!',
+                "FreezeWatch encountered an error, and has stopped.\n\n" +
+                "Stack trace:\n#{e.inspect}\n#{e.backtrace.join("\n")}")
+  raise e
 end
